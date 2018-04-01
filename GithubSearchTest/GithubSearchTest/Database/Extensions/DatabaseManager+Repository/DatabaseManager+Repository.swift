680e78d7426881452a3d79472d053297a11fafde
Alex Golub <alexeythumb@gmail.com>
2018-04-01 14:46:13 +0300

    Add blank DatabaseManager+Repository

---
+++ GithubSearchTest/GithubSearchTest/Database/Extensions/DatabaseManager+Repository/DatabaseManager+Repository.swift
@@ -0,0 +1,19 @@
+//
+//  DatabaseManager+Repository.swift
+//  GithubSearchTest
+//
+//  Created by Alex Golub on 4/1/18.
+//  Copyright © 2018 Alex Golub. All rights reserved.
+//
+
+import Foundation
+
+extension DatabaseManager {
+    func createRepositoriesFrom(response: [String : Any]) {
+        // TODO:
+    }
+
+    func fetchRepositories() {
+        // TODO:
+    }
+}
