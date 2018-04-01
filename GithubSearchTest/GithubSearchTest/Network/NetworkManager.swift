98b1934c5fcc74e0104d586bc44b31bdbde52049
Alex Golub <alexeythumb@gmail.com>
2018-04-01 15:03:16 +0300

    Add NetworkManager and basic APIJSON protocol + extension

---
+++ GithubSearchTest/GithubSearchTest/Network/NetworkManager.swift
@@ -0,0 +1,53 @@
+//
+//  NetworkManager.swift
+//  GithubSearchTest
+//
+//  Created by Alex Golub on 4/1/18.
+//  Copyright © 2018 Alex Golub. All rights reserved.
+//
+
+import Foundation
+
+class NetworkManager {
+    static let shared = NetworkManager()
+    private init() {}
+
+    fileprivate let baseURL: String = {
+        return "https://api.github.com"
+    }()
+}
+
+//extension NetworkManager: APISearch {}
+
+protocol APIJSON {
+    func GETRequest(query: String) -> [String : Any]
+}
+
+extension APIJSON {
+    func GETRequest(query: String) -> [String : Any] {
+        URLSession.shared.invalidateAndCancel()
+
+        let request = generalURLRequest(query: query)
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                // TODO: handle errors
+                return
+            }
+
+            do {
+                guard let responseObject = try? JSONSerialization.jsonObject(with: data) else {
+                    // TODO: handle errors
+                    return
+                }
+                let responseDictionary = responseObject as! [String : Any]
+                // TODO: handle response
+            }
+        }.resume()
+    }
+
+    // MARK: - Utils
+
+    private func generalURLRequest(query: String) -> URLRequest {
+        // TODO: handle URLRequest creation
+    }
+}
