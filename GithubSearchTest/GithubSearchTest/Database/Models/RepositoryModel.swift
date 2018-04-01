1256d93aa52859f33d981cb35e2f03b1b7d4624f
Alex Golub <alexeythumb@gmail.com>
2018-04-01 16:59:12 +0300

    Add RepositoryModel

---
+++ GithubSearchTest/GithubSearchTest/Database/Models/RepositoryModel.swift
@@ -0,0 +1,29 @@
+//
+//  RepositoryModel.swift
+//  GithubSearchTest
+//
+//  Created by Alex Golub on 4/1/18.
+//  Copyright © 2018 Alex Golub. All rights reserved.
+//
+
+import Foundation
+
+struct RepositoryModel {
+    let name: String
+    let description: String
+    let stars: Int
+    let forks: Int
+    let updateDate: Date
+
+    init(name: String,
+         description: String,
+         stars: Int,
+         forks: Int,
+         updateDate: Date) {
+        self.name = name
+        self.description = description
+        self.stars = stars
+        self.forks = forks
+        self.updateDate = updateDate
+    }
+}
