6950414f74e410600dec16bf1d64a26c421f976d
Alex Golub <alexeythumb@gmail.com>
2018-04-01 14:18:19 +0300

    Setup basic SearchViewController UI and layout

---
+++ GithubSearchTest/GithubSearchTest/Flows/Main/SearchViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SearchViewController.swift
+//  GithubSearchTest
+//
+//  Created by Alex Golub on 4/1/18.
+//  Copyright © 2018 Alex Golub. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+
+    // MARK: - View Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
