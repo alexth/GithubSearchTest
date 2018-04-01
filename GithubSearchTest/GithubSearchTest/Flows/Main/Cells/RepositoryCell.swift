ae37388fb7bee3c039c5b1e1f1ac9e74e6a3527c
Alex Golub <alexeythumb@gmail.com>
2018-04-01 16:51:48 +0300

    Add basic RepositoryCell

---
+++ GithubSearchTest/GithubSearchTest/Flows/Main/Cells/RepositoryCell.swift
@@ -0,0 +1,23 @@
+//
+//  RepositoryCell.swift
+//  GithubSearchTest
+//
+//  Created by Alex Golub on 4/1/18.
+//  Copyright © 2018 Alex Golub. All rights reserved.
+//
+
+import UIKit
+
+class RepositoryCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    // TODO:
+//    @IBOutlet weak var descriptionLabel: UILabel!
+//    @IBOutlet weak var starsLabel: UILabel!
+//    @IBOutlet weak var forksLabel: UILabel!
+//    @IBOutlet weak var updateDateLabel: UILabel!
+
+    func updateWith(model: RepositoryModel) {
+        // TODO:
+    }
+}
